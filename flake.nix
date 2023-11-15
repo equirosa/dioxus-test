d97c77184f2de8e1c56d34a27f8e03e56f7f5447
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-11-15 01:09:29 -0600

    init project

--- flake.nix
+++ flake.nix
@@ -25,7 +25,9 @@
             dioxus-cli
             openssl
             pkg-config
-            rust-bin.stable.latest.default
+            (rust-bin.stable.latest.default.override {
+              targets = [ "wasm32-unknown-unknown" ];
+            })
           ];
 
           shellHook = '' '';
